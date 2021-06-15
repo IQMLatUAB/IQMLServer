a47461606b8f25a6a9c6eddb08c5487a71bff2bb
Leon Chiu <a88863480@gmail.com>
2021-06-15 16:25:43 -0500

    Add BraTS-Toolkit repo

---
+++ BraTS-Toolkit/segmentation_zyx_2019.py
@@ -0,0 +1,58 @@
+"""
+ Copyright (C) 2019 Florian Kofler (florian.kofler[at]tum.de) & Christoph Berger (c.berger[at]tum.de)
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published
+ by the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+ 
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+import os
+import datetime
+
+from brats_toolkit.segmentor import Segmentor
+
+
+# log
+starttime = str(datetime.datetime.now().time())
+print("*** starting at", starttime, "***")
+
+# instantiate
+seg = Segmentor(verbose=True)
+
+# input files
+t1File = os.path.join(os.path.dirname(os.getcwd()), 'IQMLServer', 'Image_Analysis_Result', 'Output', 
+                    'BTK_preprocessor', 'Subject1', 'hdbet_brats-space', 'Subject1_hdbet_brats_t1.nii.gz')
+t1cFile = os.path.join(os.path.dirname(os.getcwd()), 'IQMLServer', 'Image_Analysis_Result', 'Output', 
+                    'BTK_preprocessor', 'Subject1', 'hdbet_brats-space', 'Subject1_hdbet_brats_t1c.nii.gz')
+t2File = os.path.join(os.path.dirname(os.getcwd()), 'IQMLServer', 'Image_Analysis_Result', 'Output', 
+                    'BTK_preprocessor', 'Subject1', 'hdbet_brats-space', 'Subject1_hdbet_brats_t2.nii.gz')
+flaFile = os.path.join(os.path.dirname(os.getcwd()), 'IQMLServer', 'Image_Analysis_Result', 'Output', 
+                    'BTK_preprocessor', 'Subject1', 'hdbet_brats-space', 'Subject1_hdbet_brats_fla.nii.gz')
+
+# output
+outputFolder = os.path.join(os.path.dirname(os.getcwd()), 'IQMLServer', 'Image_Analysis_Result', 'Output', 'BTK_segmentor') + '/'
+
+# algorithms selected for segmentation
+cid = 'zyx_2019'
+
+# execute it
+try:
+    outputFile = outputFolder + cid + ".nii.gz"
+    seg.segment(t1=t1File, t2=t2File, t1c=t1cFile,
+                    fla=flaFile, cid=cid, outputPath=outputFile)
+
+except Exception as e:
+    print("error:", str(e))
+    print("error occured for:", cid)
+
+# log
+endtime = str(datetime.datetime.now().time())
+print("*** finished at:", endtime, "***")
---
+++ BraTS-Toolkit/src/brats-toolkit/brats_toolkit/util/prep_utils.py
@@ -0,0 +1,31 @@
+"""
+ Copyright (C) 2019 Florian Kofler (florian.kofler[at]tum.de) & Christoph Berger (c.berger[at]tum.de)
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published
+ by the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+ 
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from pathlib import Path
+import shutil
+import os
+
+
+def tempFiler(orgFilePath, modality, tempFolder):
+    stemName = Path(orgFilePath).stem
+    stemName = stemName.rsplit('.', 2)[0]
+    stemName = stemName + "_" + modality + ".nii.gz"
+
+    tempFile = os.path.join(tempFolder, stemName)
+    # print("tempFile:", tempFile)
+    shutil.copyfile(orgFilePath, tempFile)
+    return tempFile
